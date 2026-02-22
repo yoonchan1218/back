9dfd2e4b15a4fb344c559b0600d6268b69da654a
JUNSU\dlwns <dlwnstn0315@gmail.com>
2026-02-22 20:52:13 +0900

    update

--- src/main/resources/static/js/qna/header-event.js
+++ src/main/resources/static/js/qna/header-event.js
@@ -38,21 +38,25 @@ if (headerFooterDetailButton) {
 
 // 기업 서비스에 마우스 올렸을 때 class open 추가 삭제
 const userCorp = document.querySelector(".userNav .corp");
-userCorp.addEventListener("mouseenter", (e) => {
-    userCorp.classList.add("open");
-});
-userCorp.addEventListener("mouseleave", (e) => {
-    userCorp.classList.remove("open");
-});
+if (userCorp) {
+    userCorp.addEventListener("mouseenter", (e) => {
+        userCorp.classList.add("open");
+    });
+    userCorp.addEventListener("mouseleave", (e) => {
+        userCorp.classList.remove("open");
+    });
 
-// 기업 서비스 안에 기업 로그인에 마우스 올렸을 때 색 변경
-const userCorpLogin = document.querySelector(".btnRowWrap a");
-userCorpLogin.addEventListener("mouseenter", (e) => {
-    e.target.style.backgroundColor = "#F8F8F8";
-});
-userCorpLogin.addEventListener("mouseleave", (e) => {
-    e.target.style.backgroundColor = "#fff";
-});
+    // 기업 서비스 안에 기업 로그인에 마우스 올렸을 때 색 변경
+    const userCorpLogin = userCorp.querySelector(".btnRowWrap a");
+    if (userCorpLogin) {
+        userCorpLogin.addEventListener("mouseenter", (e) => {
+            e.target.style.backgroundColor = "#F8F8F8";
+        });
+        userCorpLogin.addEventListener("mouseleave", (e) => {
+            e.target.style.backgroundColor = "#fff";
+        });
+    }
+}
 
 // 헤더 네비게이션 마우스 올렸을 때 open-active 클래스 추가 삭제
 const headNav = document.getElementById("headNavBar");
